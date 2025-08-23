9a905a3ca6922ec98ccf52152ae4621562df4184
strdeok <ejraks1548@gmail.com>
2025-08-23 21:04:08 +0900

    fix: props 타입 수정

--- src/app/(afterLogin)/documents/[documentId]/page.tsx
+++ src/app/(afterLogin)/documents/[documentId]/page.tsx
@@ -1,11 +1,11 @@
 import DocumentDescription from "./_components/documentDescription";
 import PdfViewer from "./_components/previewDocuments";
 
-export default function DetailedDocumentsPage({
-  params,
-}: {
+type Props = {
   params: { documentId: string };
-}) {
+};
+
+export default function DetailedDocumentsPage({ params }: Props) {
   console.log(params.documentId);
   return (
     <div className="flex flex-row pb-24 mt-8 gap-10">
