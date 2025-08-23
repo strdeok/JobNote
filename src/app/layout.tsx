9d4c970d013f284f2b5b516d3b0626be81ce8b0d
strdeok <ejraks1548@gmail.com>
2025-08-23 20:23:15 +0900

    fix: tanstack query 오류 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { queryClient } from "@/lib/queryclient";
+import ClientProvider from "./clientProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,9 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <QueryClientProvider client={queryClient}>
-          {children}
-        </QueryClientProvider>
+        <ClientProvider>{children}</ClientProvider>
       </body>
     </html>
   );
