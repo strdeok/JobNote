8deb3a5e0518f9232b0c14dd330e2bdbe873c8a3
strdeok <ejraks1548@gmail.com>
2025-09-05 19:03:47 +0900

    build: 빌드 오류 수정

---
+++ src/app/set-nickname/_components/inputNickname.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { useState } from "react";
+import { useSocialSignUp } from "@/hooks/useAuth";
+
+export default function InputNicknamePage({ email }: { email: string }) {
+  const [nickname, setNickname] = useState("");
+
+  const mutation = useSocialSignUp();
+
+  const handleSubmit = () => {
+    if (!nickname) return;
+
+    mutation.mutate(
+      { email: email, nickname: nickname },
+      {
+        onSuccess: () => {
+          window.location.replace("/dashboard");
+        },
+        onError: (error) => {
+          console.error(error);
+          alert("오류가 발생하였습니다.");
+        },
+      }
+    );
+  };
+
+  return (
+    <div className="w-96 h-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col items-center justify-center gap-4">
+      <input
+        type="text"
+        placeholder="닉네임을 입력해주세요."
+        className="w-full border border-[#D9D9D9] py-1 rounded-xs pl-2"
+        value={nickname}
+        onChange={(e) => setNickname(e.target.value)}
+      />
+      <button
+        className="w-full bg-main rounded-xs text-white py-1"
+        onClick={handleSubmit}
+      >
+        등록하기
+      </button>
+    </div>
+  );
+}
--- src/app/set-nickname/page.tsx
+++ src/app/set-nickname/page.tsx
@@ -1,48 +1,10 @@
-"use client";
+import InputNicknamePage from "./_components/inputNickname";
 
-import { useState } from "react";
-import { useSearchParams } from "next/navigation";
-import { useSocialSignUp } from "@/hooks/useAuth";
+interface PageProps {
+  searchParams: { [key: string]: string | string[] | undefined };
+}
 
-export default function SetNicknamePage() {
-  const searchParams = useSearchParams();
-  const email = searchParams.get("email") || "";
-  const [nickname, setNickname] = useState("");
-
-  const mutation = useSocialSignUp();
-
-  const handleSubmit = () => {
-    if (!nickname) return;
-
-    mutation.mutate(
-      { email: email, nickname: nickname },
-      {
-        onSuccess: () => {
-          window.location.replace("/dashboard");
-        },
-        onError: (error) => {
-          console.error(error);
-          alert("오류가 발생하였습니다.");
-        },
-      }
-    );
-  };
-
-  return (
-    <div className="w-96 h-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col items-center justify-center gap-4">
-      <input
-        type="text"
-        placeholder="닉네임을 입력해주세요."
-        className="w-full border border-[#D9D9D9] py-1 rounded-xs pl-2"
-        value={nickname}
-        onChange={(e) => setNickname(e.target.value)}
-      />
-      <button
-        className="w-full bg-main rounded-xs text-white py-1"
-        onClick={handleSubmit}
-      >
-        등록하기
-      </button>
-    </div>
-  );
+export default function SetNicknamePage({ searchParams }: PageProps) {
+  const email = searchParams.email as string;
+  return <InputNicknamePage email={email} />;
 }
